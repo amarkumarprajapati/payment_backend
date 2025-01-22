75a4b4d83c9f6ea8c63446dd9179a627ec3c29c8
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2025-01-22 16:59:34 +0530

    added to know my server is running

--- app.js
+++ app.js
@@ -7,29 +7,31 @@ const app = express();
 const port = 5000;
 
 app.use(cors());
-
-app.get("/get/qr", async (req, res) => {
-    try {
-        const upiID = "8908013842@pthdfc"; 
-        const payeeName = "Amar Kumar"; 
-        const amount = 1;
-        const currency = "INR"; 
-        const qrCodeText = paytmurl(upiID,payeeName,amount,currency)
-        const qrCodeDataURL = await QRCode.toDataURL(qrCodeText);
-        res.json({
-            success: true,
-            qrCodeDataURL: qrCodeDataURL,
-        });
-        console.log(qrCodeDataURL)
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: "Failed to generate QR Code",
-        });
-    }
+app.get("/", async (req, res) => {
+  res.send("i am running");
 });
 
+app.get("/get/qr", async (req, res) => {
+  try {
+    const upiID = "8908013842@pthdfc";
+    const payeeName = "Amar Kumar";
+    const amount = 1;
+    const currency = "INR";
+    const qrCodeText = paytmurl(upiID, payeeName, amount, currency);
+    const qrCodeDataURL = await QRCode.toDataURL(qrCodeText);
+    res.json({
+      success: true,
+      qrCodeDataURL: qrCodeDataURL,
+    });
+    console.log(qrCodeDataURL);
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to generate QR Code",
+    });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
